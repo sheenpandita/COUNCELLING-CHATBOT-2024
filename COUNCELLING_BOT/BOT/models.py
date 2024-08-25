ec31e9685d08853c4aeff9a0594b9dbf35390146
Sheen Pandita <84227466+sheenpandita@users.noreply.github.com>
2024-08-25 10:13:15 +0000

    database_login defining first stage of creating user login and registration ..and creating dashboard for each user

--- COUNCELLING_BOT/BOT/models.py
+++ COUNCELLING_BOT/BOT/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+# Extending User Model Using a One-To-One Link
+class User(models.Model):
+    name = models.CharField(max_length=70)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
